0edac27074814bd0ebdeb8eaa806f16c72d1494f
fl-armable <fl.irlanda@gmail.com>
2025-04-28 06:10:02 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+    //eligiendo elementos aleatoriamente
+  let rndm1 = pronoun[Math.floor(Math.random()*pronoun.length)];
+  let rndm2 = adj[Math.floor(Math.random()*adj.length)];
+  let rndm3 = noun[Math.floor(Math.random()*noun.length)] + ".com";
+  //enviando al HTML mediante id = excuse
+  document.querySelector("#domain").innerHTML = rndm1 + rndm2 + rndm3;
 };
